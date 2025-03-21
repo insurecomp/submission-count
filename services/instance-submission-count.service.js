33d183a8c331abf16dfd2087bc983de4493dfcb2
SurajS10dulkar <surajtendulkar@insurecomp.com>
2025-03-21 05:24:46 -0400

    Added Premium Coloum

--- services/instance-submission-count.service.js
+++ services/instance-submission-count.service.js
@@ -5,8 +5,14 @@ const {
   QueryCommand,
 } = require("@aws-sdk/lib-dynamodb");
 const moment = require("moment");
-const client = new DynamoDBClient({ region: "us-east-1" }); // Set your region
+const client = new DynamoDBClient({ region: "us-east-1" });
+const { GetObjectCommand, PutObjectCommand, HeadObjectCommand, S3Client } = require("@aws-sdk/client-s3");
 const docClient = DynamoDBDocumentClient.from(client);
+const s3Client = new S3Client({
+  region: "us-east-1",
+});
+const LOOP_LIMIT = 3;
+const BUCKET_NAME = "e3-quote-json-docs";
 
 class instanceCountService {
   constructor() {}
@@ -55,20 +61,101 @@ class instanceCountService {
 
   //------------E3 Start Here----------------------//
 
-  //calculate Payroll
-  getE3TotalPremium = (obj) => {
-    let sum = 0;
-    for (const key in obj) {
-      sum += obj[key].total_standard_premium;
-    }
-    return sum;
+  async hasFileGenerated(params) {
+  try {
+
+    const command = new HeadObjectCommand(params);
+    await s3Client.send(command);
+
+    return true;
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
+  }
+
+  streamToString = (stream) => {
+  return new Promise((resolve, reject) => {
+    const chunks = [];
+    stream.on("data", (chunk) => chunks.push(Buffer.from(chunk)));
+    stream.on("end", () => resolve(Buffer.concat(chunks).toString("utf-8")));
+    stream.on("error", reject);
+  });
   };
+
+  async loopOverBucketAndCheckForFile(params, i = 0) {
+    let returnVal = false;
+    await new Promise((resolve, reject) => {
+  
+      if (i >= LOOP_LIMIT) reject("Error");
+  
+      setTimeout(async () => {
+        let fileExists = this.hasFileGenerated(params);
+  
+        if (fileExists) resolve("found");
+        else if (i < LOOP_LIMIT) resolve("continue");
+        else resolve("not found");
+      }, 3000);
+    })
+      .then(res => {
+        console.log("res: ", res);
+        if (res === "not found") {
+          returnVal = false;
+        } else if (res === "continue") {
+          returnVal = this.loopOverBucketAndCheckForFile(params, i + 1);
+        } else if (res === "found") {
+          returnVal = true;
+        }
+      })
+      .catch(err => { console.log(err); returnVal = false; });
+    return returnVal;
+  }
+  
+  async fetchFroms3  (key) {
+    try {
+      const PARAM_TO_BUCKET = {
+        Bucket: BUCKET_NAME,
+        Key: key,
+      };
+  
+      let checkFile = await this.loopOverBucketAndCheckForFile(PARAM_TO_BUCKET, 0);
+  
+      if (!checkFile) throw new Error("File doesnot exist");
+  
+      const command = new GetObjectCommand(PARAM_TO_BUCKET);
+  
+      const data = await s3Client.send(command);
+      const jsonQuotesData = await this.streamToString(data?.Body);
+  
+      return { data: jsonQuotesData, error: null };
+  
+    } catch (error) {
+      console.log(error);
+      return { data: null, error: "Error" }
+    }
+  }
+
+  getE3TotalPremium =async (obj,key)=>{
+    if (obj?.["keyToStorage"]) {
+      let { data, error } = await this.fetchFroms3(obj["keyToStorage"]);
+      let dataKey=key.split("+")[1]
+      // console.log("Suraj 0098=====>",JSON.parse(data)?.[dataKey]?.["e3hr"])
+      if (error) {
+        console.log(error);
+        return null
+      };
+      return JSON.parse(data)?.[dataKey]?.["e3hr"];
+    } else {
+      return obj;
+    }
+  }
 
   // Fetch E3 HR Data from User Status Data
   fetchE3UserStatusData = async (partitionKey) => {
     if (!partitionKey) {
       return 0;
     }
+    let selectedCarrier= await this.fetchE3CarrierSelected(partitionKey)
     const params = {
       TableName: "E3UserStatusTable",
       KeyConditionExpression: "#id = :user_email_id",
@@ -82,12 +169,16 @@ class instanceCountService {
       Limit: 1,
     };
     try {
-      const data = await docClient.send(new QueryCommand(params)); // Use QueryCommand with send()
+      const data = await docClient.send(new QueryCommand(params));
       if (data.Items[0]?.carrier_location_data) {
-        const totalPremium = this.getE3TotalPremium(
-          data.Items[0].carrier_location_data
+        const jsonData = await this.getE3TotalPremium(
+          data.Items[0].carrier_location_data, data.Items[0].uuid_carrier
         );
-        return totalPremium;
+        let teap;
+        if(jsonData&&selectedCarrier){
+          teap=jsonData[selectedCarrier]?.total_estimated_annual_premium||0
+        }
+        return teap;
       } else {
         return 0;
       }
@@ -101,11 +192,7 @@ class instanceCountService {
     if (!partitionKey) {
       return 0;
     }
-    const carrierMap = {
-      carrier_az: "Arch",
-      carrier_ba: "SUNZ",
-      carrier_bc: "Prescient",
-    };
+
     const params = {
       TableName: "E3UserStatusTable",
       KeyConditionExpression: "#id = :user_email_id",
@@ -121,9 +208,9 @@ class instanceCountService {
     try {
       const data = await docClient.send(new QueryCommand(params));
       if (data.Items[0]?.tableCarrierSelect) {
-        const carrier = carrierMap[data.Items[0]?.tableCarrierSelect]
-          ? carrierMap[data.Items[0]?.tableCarrierSelect]
-          : data.Items[0]?.tableCarrierSelect;
+        const carrier = data.Items[0]?.tableCarrierSelect
+          ? data.Items[0]?.tableCarrierSelect
+          : "Null";
         return carrier;
       } else {
         return "NULL";
@@ -161,6 +248,11 @@ class instanceCountService {
   //E3 Coloumn & Row setting
   e3RowCalculate = async (item) => {
     const obj = {};
+    const carrierMap = {
+      carrier_az: "Arch",
+      carrier_ba: "SUNZ",
+      carrier_bc: "Prescient",
+    };
     obj["Unique Id"] = item?.user_email_id;
     obj["CompanyName"] = item?.companyProfile?.companyName?.value || "";
     obj["FEIN"] = item?.companyProfile?.fein?.value || "";
@@ -203,9 +295,10 @@ class instanceCountService {
     obj["QuoteDate"] = item?.quoteData?.date
       ? moment(item?.quoteData?.date, ["x"]).format("MM-DD-YYYY")
       : "NULL";
-    obj["SelectedCarrier"] = await this.fetchE3CarrierSelected(
+     let selectedCarrier= await this.fetchE3CarrierSelected(
       item?.user_email_id
     );
+    obj["SelectedCarrier"]=selectedCarrier? carrierMap[selectedCarrier]:selectedCarrier
     console.log(obj);
     return obj;
   };
@@ -348,6 +441,15 @@ class instanceCountService {
     obj["Created Date"] = item?.createdDate
       ? moment(item?.createdDate, ["x"]).format("MM-DD-YYYY")
       : "";
+
+    let lossRunData=await fetchIESpibitOCR(item?.user_email_id)
+    if(lossRunData){
+      obj["LossRunUpload"]="Yes"
+      obj["LossRunDate"] = lossRunData
+    }else{
+      obj["LossRunUpload"]="No"
+      obj["LossRunDate"] ="NULL"
+    }
     console.log(obj);
     return obj;
   };
