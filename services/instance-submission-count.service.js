96fd9b383995e8a6a638a3c6d953cf9df26f778a
SurajS10dulkar <surajtendulkar@insurecomp.com>
2025-04-23 03:02:55 -0500

    Added revised date change format

--- services/instance-submission-count.service.js
+++ services/instance-submission-count.service.js
@@ -236,8 +236,8 @@ class instanceCountService {
       const response = await docClient.send(command);
       const date = response.Items[0]?.createdTimestamp;
       const e3CreatedDate = date
-        ? moment(date + "000", ["x"]).format("MM-DD-YYYY")
-        : "";
+      ? moment.unix(date).format("MM-DD-YYYY")
+      : "";
       return e3CreatedDate;
     } catch (error) {
       console.error("Error fetching items:", error);
